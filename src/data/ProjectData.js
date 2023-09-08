287f9d97ee2b1e1c092ce29b4d61aba30c7ff315
Marcel Adamski <marcel.adamski@redge.com>
2023-09-08 11:05:07 +0200

    Initial commit

--- src/data/ProjectData.js
+++ src/data/ProjectData.js
@@ -52,7 +52,7 @@ export const stackList = [
     name: "Python",
   },
   {
-    img: "https://raw.githubusercontent.com/MarcelAdamski/portfolio/main/src/Assets/docker1.png",
+    img: "https://raw.githubusercontent.com/MarcelAdamski/portfolio/main/src/Assets/docker.png",
     name: "Docker",
   },
   {
@@ -64,7 +64,7 @@ export const stackList = [
     name: "Terraform",
   },
   {
-    img: "https://raw.githubusercontent.com/MarcelAdamski/portfolio/main/src/Assets/jenkins1.png",
+    img: "https://raw.githubusercontent.com/MarcelAdamski/portfolio/main/src/Assets/jenkins.png",
     name: "Jenkins",
   },
   {
